b165d94e9baedf299a6b6f02451398fab3820596
Jing Cao <jingcaomath@gmail.com>
2026-07-06 17:48:36 -0400

    add self-RAG

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -7,6 +7,7 @@ load_dotenv()
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from ingestion import retriever
 from graph.chains.generation import generation_chain
+from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
 
 
 
@@ -45,4 +46,25 @@ def test_generation_chain() -> None:
     pprint(generation)
 
 
-    
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score == 'yes'
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs, 
+            "generation": "In order to make pizza, we need to first start with the dough."
+        }
+    )
+    assert res.binary_score == 'no'
--- graph/graph.py
+++ graph/graph.py
@@ -4,6 +4,8 @@ from langgraph.graph import END, StateGraph
 from graph.consts import RETRIEVE, GENERATE, GRADE_DOCUMENTS, WEB_SEARCH
 from graph.nodes import retrieve, generate, grade_documents, web_search
 from graph.state import AgentState
+from graph.chains.answer_grader import answer_grader
+from graph.chains.hallucination_grader import hallucination_grader
 
 load_dotenv()
 
@@ -17,6 +19,32 @@ def decide_to_generate(state: AgentState):
         print("---DECISION: GENERATE---")
         return GENERATE
     
+def grade_generation_grounded_in_documents_and_question(state: AgentState) -> str:
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+
+    if score.binary_score == 'yes':
+        print("---DECISION: GENERAION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke(
+            {"question": question, "generation": generation}
+        )
+        if score.binary_score == 'yes':
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESSES QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERAION IS NOT GROUNDED IN DOCUMENTS, REGENERATE AGAIN---")
+        return "not supported"
+
 builder = StateGraph(AgentState)
 
 builder.add_node(RETRIEVE, retrieve)
@@ -35,10 +63,20 @@ builder.add_conditional_edges(GRADE_DOCUMENTS,
                               }
 )
 
+builder.add_conditional_edges(
+    GENERATE,
+    grade_generation_grounded_in_documents_and_question,
+    path_map={
+        "not supported": GENERATE,
+        "useful": END,
+        "not useful": WEB_SEARCH,
+    }   
+)
+
 builder.add_edge(WEB_SEARCH, GENERATE)
-builder.add_edge(GENERATE, END)
+#builder.add_edge(GENERATE, END)
 
 workflow = builder.compile()
 
-workflow.get_graph().draw_mermaid_png(output_file_path="graph.png")
+workflow.get_graph().draw_mermaid_png(output_file_path="graph2.png")
 
